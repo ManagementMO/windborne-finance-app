40dfa4f14adf9bf7e1b9b3fda9628da5deb7bf70
Mohammed Elshrief <elshriefmoh@gmail.com>
2025-09-22 19:26:39 -0400

    deleted old frontend

--- web/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-// src/lib/api.ts
-import axios from 'axios';
-
-export const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // the FastAPI backend URL
-});
--- web/src/types/vendors.ts
+++
@@ -1,9 +0,0 @@
-export type VendorOverview = {
-symbol: string;
-name: string;
-description: string;
-market_cap: number;
-pe_ratio: number;
-dividend_yield: number;
-ebitda: number;
-};
